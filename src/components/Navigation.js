8d5a1ce5141273552cf6e072594dd1723b68f210
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2024-05-02 18:59:36 -0500

    navigation styled and conditional rendering

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,14 +1,34 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+import './Navigation.css'
+
 export default function Navigation() {
+    const token = localStorage.getItem('token') // check if the user is logged in
+
+    const handleLogout = () => {
+        localStorage.removeItem('token') // remove the token from localStorage
+        window.location.href = "/" // redirect to the home page
+    }
+
     return (
         <nav>
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/register">Register</Link></li>
-                <li><Link to="/login">Login</Link></li>
+            <ul className='nav-list'>
+                <li className='nav-item'><Link to="/">Home</Link></li>
+                <li className='nav-item'><Link to="/about">About</Link></li>
+
+                {token ? (
+                    <>
+                        <li className='nav-item'><Link to="/admin">Admin</Link></li>
+                        <li className='nav-item'><button className='logout-button' onClick={handleLogout}>Logout</button></li>
+                    </>
+                ) : (
+                    <>
+                        <li className='nav-item'><Link to="/register">Register</Link></li>
+
+                        <li className='nav-item'><Link to="/login">Login</Link></li>
+                    </>
+                )}
             </ul>
         </nav>
     )
