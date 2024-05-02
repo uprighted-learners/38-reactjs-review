60ffbe6a6ff580f0400b66828d51406c4bea331c
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2024-05-02 18:14:18 -0500

    register page working

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,7 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 export default function Register() {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const navigate = useNavigate()
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            const response = await fetch(`https://three2-mongodb-review.onrender.com/api/users`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    username,
+                    password
+                })
+            })
+            setUsername('')
+            setPassword('')
+            alert('User created successfully!')
+            navigate('/login')
+        } catch (error) {
+            alert(error.message)
+        }
+    }
+
     return (
-        <div>Register</div>
+        <div>
+            <h2>Register New User</h2>
+            <form onSubmit={handleSubmit}>
+                <label>Username:</label>
+                <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} required />
+                <label>Password:</label>
+                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+                <button type="submit">Register</button>
+                <p>Already have an account? <Link to="/login">Login here</Link></p>
+            </form>
+        </div>
     )
 }
